a6f7e48b51f4cebe560d8a47686a29b8bf907ebb
andrewnewguy <andrey.dclink@gmail.com>
2022-08-24 14:55:57 +0300

    added 404 about contact

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ gulp.task('style', () => {
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/lib/dist/jquery.rateyo.css',
-        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -46,6 +47,7 @@ gulp.task('script', () => {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/lib/dist/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
