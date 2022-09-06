aab561e75f34eecb5ec27ed18300cf84f3173d67
andrewnewguy <andrey.dclink@gmail.com>
2022-09-06 22:20:56 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,42 @@
 $(function () {
 
     $(".rate-star").rateYo({
+
         rating: 5,
         starWidth: "13px",
         readOnly: true
     });
 
     $('.product-slider__inner').slick({
+
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [{
 
+            breakpoint: 1900,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
     });
 
     $(".js-range-slider").ionRangeSlider({
